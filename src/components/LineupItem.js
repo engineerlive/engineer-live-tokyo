e8822df35e3e396109854199aea3457d430df37e
Hirofumi Wakasugi <baenej@gmail.com>
2018-09-22 16:50:01 +0900

    Improve lineup page

--- src/components/LineupItem.js
+++ src/components/LineupItem.js
@@ -1,11 +1,24 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import styled from "styled-components";
+
+const LineupItemWrapper = styled.div`
+  border-bottom: solid 1px #3a3a3a;
+  margin: 2em 0;
+  padding: 2em 1em;
+`;
+
+const LineupTitle = styled.h2`
+  font-size: 2rem;
+  line-height: 4rem;
+  text-align: center;
+`;
 
 export default ({ title, issueNumber, bodyHTML }) => (
-  <div>
-    <h2>
+  <LineupItemWrapper>
+    <LineupTitle>
       {issueNumber ? <Link to={`/lineup/${issueNumber}`}>{title}</Link> : title}
-    </h2>
+    </LineupTitle>
     <div dangerouslySetInnerHTML={{ __html: bodyHTML }} />
-  </div>
+  </LineupItemWrapper>
 );
