16579d5851a88c98f5e9f9849988e83da938e544
Hirofumi Wakasugi <baenej@gmail.com>
2018-10-21 14:24:47 +0900

    Add prerendering support to Home

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -5,19 +5,28 @@ import LineupList from "./LineupList"
 import PostList from "./PostList"
 import TopInformation from "./TopInformation"
 
-const Home = () => (
-  <div>
-    <TopInformation />
-    <div id="about">
-      <About summary={true} />
+declare var window: {
+  prerenderReady: boolean
+}
+
+const Home: React.SFC<{}> = () => {
+  // Prerendering support
+  window.prerenderReady = true
+
+  return (
+    <div>
+      <TopInformation />
+      <div id="about">
+        <About summary={true} />
+      </div>
+      <div id="lineup">
+        <LineupList />
+      </div>
+      <div id="news">
+        <PostList numberOfPosts={10} />
+      </div>
     </div>
-    <div id="lineup">
-      <LineupList />
-    </div>
-    <div id="news">
-      <PostList numberOfPosts={10} />
-    </div>
-  </div>
-)
+  )
+}
 
 export default Home
